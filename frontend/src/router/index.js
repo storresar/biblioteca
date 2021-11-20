a1b7412a90a5aa82816d1e5a0b97514d90fba1e1
storresar <50678254+storresar@users.noreply.github.com>
2021-11-19 21:59:43 -0500

    Inicio pagina admin

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Admin from '../views/Admin.vue'
 
 const routes = [
   {
@@ -14,6 +15,10 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/admin',
+    component: Admin
   }
 ]
 
@@ -21,5 +26,7 @@ const router = createRouter({
   history: createWebHashHistory(),
   routes
 })
+
+
 
 export default router
---
+++ frontend/src/views/Admin.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="admin">
+        <Sidebar/>
+        <h1>dsadsdsa</h1>
+    </div>
+</template>
+
+
+<script>
+import Sidebar from '@/components/Sidebar.vue'
+
+
+export default {
+    name: 'Admin',
+    components: {
+        Sidebar,
+    }
+}
+</script>
