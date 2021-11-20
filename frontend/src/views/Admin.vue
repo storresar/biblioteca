1de7a6820ed0f70dd90252307316b1b238f393b6
storresar <50678254+storresar@users.noreply.github.com>
2021-11-19 23:06:43 -0500

    Cambios pagina admin

--- frontend/src/views/Admin.vue
+++ frontend/src/views/Admin.vue
@@ -1,19 +1,23 @@
 <template>
     <div class="admin">
         <Sidebar/>
-        <h1>dsadsdsa</h1>
+        <NavbarUser/>
+        <h1>DJSKHSAJDKLHSJKL</h1>
+        <button class="ml-2 inline-flex items-center justify-center  h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-red-350 hover:bg-purple-accent-700">DSKALJD </button>
     </div>
 </template>
 
 
 <script>
 import Sidebar from '@/components/Sidebar.vue'
+import NavbarUser from '@/components/NavbarUser.vue'
 
 
 export default {
     name: 'Admin',
     components: {
         Sidebar,
+        NavbarUser
     }
 }
 </script>
