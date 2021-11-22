ad3cf948aadf4841b0fe73616b37f1e0f2694b66
Geeksebas <sebastianruizcelis@gmail.com>
2021-11-21 21:44:28 -0500

    Registro medio troll

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Registro_Usuarios from '../views/Registro_Usuarios.vue'
 
 const routes = [
   {
@@ -14,9 +15,13 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/registro',
+    name: 'Registro',
+    component: Registro_Usuarios
   }
 ]
-
 const router = createRouter({
   history: createWebHashHistory(),
   routes
---
+++ frontend/src/views/Registro_Usuarios.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="registro">
+
+    <Registro/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Registro from '@/components/Registro.vue'
+
+export default {
+  name: 'Registro',
+  components: {
+    Registro,
+  },
+}
+</script>
